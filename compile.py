25efe3a0daa9775817fe301d7d8f858fbae86ea4
Guilherme Daros <gh5777.gh@gmail.com>
2023-12-19 21:44:22 -0300

    modify script to work with beacon

--- compile.py
+++ compile.py
@@ -13,32 +13,29 @@ for lw in line_width:
         for dw in data_width:
             print(f"compiling lw{lw}_md{md}_{dw}.elf\n")
             
-            with open("./apps/tcc_final/src/models/CMakeLists.txt", "w+") as file:
-                file.write(f'''target_sources(tcc_final PUBLIC ${{CMAKE_CURRENT_LIST_DIR}}/lw{lw}_md{md}.cc)''')
+            with open("./apps/tcc_beacon/src/models/CMakeLists.txt", "w+") as file:
+                file.write(f'''target_sources(tcc_beacon PUBLIC ${{CMAKE_CURRENT_LIST_DIR}}/lw{lw}_md{md}.cc)''')
 
-            with open("./apps/tcc_final/main.cpp", "r") as file:
+            with open("./apps/tcc_beacon/main.cpp", "r") as file:
                 data = file.read()
                 data = data.replace(last_model, f"lw{lw}_md{md}")             
-            with open("./apps/tcc_final/main.cpp", "w") as file:
+            with open("./apps/tcc_beacon/main.cpp", "w") as file:
                 file.write(data)
 
-            with open("./apps/tcc_final/src/server_functions.cpp", "r") as file:
+            with open("./apps/tcc_beacon/main.cpp", "r") as file:
                 data = file.read()
                 data = data.replace(last_dw, str(dw)) 
-            with open("./apps/tcc_final/src/server_functions.cpp", "w") as file:
+            with open("./apps/tcc_beacon/main.cpp", "w") as file:
                 file.write(data)
 
             subprocess.run(f'cmake --build ./build --config Release --target install -j 10 --'.split(' '),
                                   stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-            subprocess.run(f'mv ./images/tcc_final.elf ./images/tcc_lw{lw}_md{md}_{dw}.elf'.split(' '),
+            subprocess.run(f'mv ./images/tcc_beacon.elf ./images/tcc_lw{lw}_md{md}_{dw}.elf'.split(' '),
                                   stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             
             last_dw = str(dw)
             last_model = f"lw{lw}_md{md}"
-
-            print(f"last -> {last_model}_{last_dw}.elf")
-
 
 
             
